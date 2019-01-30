3fbe0bfd7b7ccc874c89d0a336cad8ca9ff257b6
Dragos Nedelcu <dragos199993@gmail.com>
2019-01-30 22:09:45 +0200

    route change.

--- dist/routes/routes.js
+++ dist/routes/routes.js
@@ -7,36 +7,33 @@ class Routes {
         this.contactController = new crmController_1.ContactController();
     }
     routes(app) {
-        app.route("*", (req, res) => {
-            res.sendFile(path.join(__dirname, "..", "client/build/index.html"));
-        });
-        app.route("/")
+        app.route("/api/")
             .get((req, res) => {
             res.status(200).send({
                 message: "GET request succesfully"
             });
         });
-        app.route("/")
+        app.route("/api/")
             .post((req, res) => {
             res.status(200).send({
                 message: "POST request succesfully"
             });
         });
-        app.route("/")
+        app.route("/api/")
             .put((req, res) => {
             res.status(200).send({
                 message: "PUT request succesfully"
             });
         });
-        app.route("/")
+        app.route("/api/")
             .delete((req, res) => {
             res.status(200).send({
                 message: "DELETE request succesfully"
             });
         });
-        app.route("/contact")
+        app.route("/api/contact")
             .post(this.contactController.addNewContact);
-        app.route("/contact")
+        app.route("/api/contact")
             .get((req, res, next) => {
             if (req.query.key !== "abe97787ac28b755ad2857a93f44721ee067ded2") {
                 res.status(401).send("Key incorrect");
@@ -45,10 +42,13 @@ class Routes {
                 next();
             }
         }, this.contactController.getContacts);
-        app.route("/contact/:contactId")
+        app.route("/api/contact/:contactId")
             .get(this.contactController.getContactWithID)
             .put(this.contactController.updateContact)
             .delete(this.contactController.deleteContact);
+        app.route("*", (req, res) => {
+            res.sendFile(path.join(__dirname, "..", "client/build/index.html"));
+        });
     }
 }
 exports.Routes = Routes;
--- lib/routes/routes.ts
+++ lib/routes/routes.ts
@@ -7,38 +7,35 @@ export class Routes {
     public contactController: ContactController = new ContactController();
 
     public routes(app): void {
-        app.route("*", (req: Request, res: Response) => {
-            res.sendFile(path.join(__dirname, "..", "client/build/index.html"));
-        });
-        app.route("/")
+        app.route("/api/")
             .get((req: Request, res: Response) => {
                 res.status(200).send({
                     message: "GET request succesfully"
                 });
             });
-        app.route("/")
+        app.route("/api/")
             .post((req: Request, res: Response) => {
                 res.status(200).send({
                     message: "POST request succesfully"
                 });
             });
-        app.route("/")
+        app.route("/api/")
             .put((req: Request, res: Response) => {
                 res.status(200).send({
                     message: "PUT request succesfully"
                 });
             });
-        app.route("/")
+        app.route("/api/")
             .delete((req: Request, res: Response) => {
                 res.status(200).send({
                     message: "DELETE request succesfully"
                 });
             });
 
-        app.route("/contact")
+        app.route("/api/contact")
             .post(this.contactController.addNewContact);
 
-        app.route("/contact")
+        app.route("/api/contact")
             .get((req: Request, res: Response, next: NextFunction) => {
                 if (req.query.key !== "abe97787ac28b755ad2857a93f44721ee067ded2") {
                     res.status(401).send("Key incorrect");
@@ -47,9 +44,13 @@ export class Routes {
                 }
             }, this.contactController.getContacts);
 
-        app.route("/contact/:contactId")
+        app.route("/api/contact/:contactId")
             .get(this.contactController.getContactWithID)
             .put(this.contactController.updateContact)
             .delete(this.contactController.deleteContact);
+
+        app.route("*", (req: Request, res: Response) => {
+            res.sendFile(path.join(__dirname, "..", "client/build/index.html"));
+        });
     }
 }
